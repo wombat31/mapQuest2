1ad6c266842ca9cafb23fb9149bb544c6ef7b460
Justin Welham <jwelham@BCAA.AE>
2020-02-13 19:10:57 +0400

    first Commit

---
+++ mapquest.js
@@ -0,0 +1,352 @@
+//CONSTANTS
+map = [
+    "ABBBBBBBBBBBBBBBBBBBBBBBC",
+    "D.......................E",
+    "D..ABC...RST............E",
+    "D..D.E..................E",
+    "D..FGH....X.............E",
+    "D.......................E",
+    "D.....ABC...............E",
+    "D.....D.E...............E",
+    "D.....FGH...............E",
+    "D.......................E",
+    "D.......................E",
+    "D.......................E",
+    "D.......................E",
+    "D.......................E",
+    "FGGGGGGGGGGGGGGGGGGGGGGGH"]
+
+var CANVAS_WIDTH = 450;
+var CANVAS_HEIGHT = 300;
+var TILESIZE = 30;
+var ARCHIEHEIGHT = 15;
+var ARCHIEWIDTH = 15;
+    
+
+//SETUP
+var canvas = document.createElement('canvas');
+var c = canvas.getContext('2d');
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+document.body.appendChild(canvas);
+var archieX = 200;
+var archieY = 130;
+var mapX = 0;
+var mapY = 0;
+var relativeX = 0;
+var relativeY = 0;
+var direction = "";
+var tileX;
+var tileY;
+var frameCount = 0;
+
+//var leftKeyPressed = false;
+//var rightKeyPressed = false;
+//var upKeyPressed = false;
+//var downKeyPressed = false;
+var keyState = {};
+
+var legalMove = true;
+
+var mapImage = new Image();
+mapImage.src = 'images/backgroundTilesPyramid.png';
+var archieImage = new Image();
+archieImage.src = 'images/archie16x16.png';
+
+window.addEventListener('load',start);
+
+window.addEventListener('keydown', onKeyDown);
+window.addEventListener('keyup', onKeyUp);
+
+function checkMove(Xpos, Ypos, dir){
+    var xIndex;
+    var yIndex;
+    console.log("Dir: "+ dir)
+    console.log("relX: " + Xpos);
+    console.log("relY: " + Ypos);
+    topLeftYIndex = Math.floor(Ypos/30);
+    
+    bottomRightYIndex = Math.floor((Ypos+ARCHIEHEIGHT)/30);
+    topLeftXIndex = Math.floor(Xpos/30);
+    bottomRightXIndex = Math.floor((Xpos+ARCHIEWIDTH)/30);
+    console.log("topLeftYIndex: " + topLeftYIndex);
+    console.log("topLeftXIndex: " + topLeftXIndex);
+    console.log("bottomRightXIndex: " + bottomRightXIndex);
+    console.log("bottomRightYIndex: " + bottomRightYIndex);
+    if(map[topLeftYIndex][topLeftXIndex] != "." || map[bottomRightYIndex][bottomRightXIndex] != "." || map[topLeftYIndex][bottomRightXIndex] != "." || map[bottomRightYIndex][topLeftXIndex] != "."){
+        legalMove = false;
+        if(dir == "S"){
+            archieY -= 5;
+            legalMove = true;
+        } else if (dir == "N"){
+            archieY += 5;
+            legalMove = true;
+        } else if(dir == "E"){
+            archieX -= 5;
+            legalMove = true;
+        } else if(dir == "W"){
+            archieX += 5;
+            legalMove = true;
+        } else if(dir == "NW"){
+            archieX += 5;
+            archieY += 5;
+            legalMove = true;
+        } else if(dir == "NE"){
+            archieX -=5;
+            archieY +=5;
+            legalMove = true;
+        } else if (dir == "SW"){
+            archieX += 5;
+            archieY -=5;
+            legalMove = true;
+        } else if (dir == "SE"){
+            archieX -=5;
+            archieY -=5;
+            legalMove = true;
+        } 
+        
+    } else {
+        legalMove = true;
+    }
+    console.log("legal Move: " + legalMove);
+    
+}
+
+function start(){
+    window.requestAnimationFrame(mainLoop);
+}
+
+
+function mapConverter(tileRef){
+    switch(tileRef){
+        case "A":
+            tileX = 0;
+            tileY = 0;
+            break;
+        case "B":
+            tileX = 30;
+            tileY = 0;
+            break;
+        case "C":
+            tileX = 60;
+            tileY = 0;
+            break;
+        case "X":
+            tileX = 90;
+            tileY = 0;
+            break;
+        case "R":
+            tileX = 120;
+            tileY = 0;
+            break;
+        case "S":
+            tileX = 150;
+            tileY = 0;
+            break;
+        case "T":
+            tileX = 180;
+            tileY = 0;
+            break;
+        case "D":
+            tileX = 0;
+            tileY = 30;
+            break;
+        case ".":
+            tileX = 30;
+            tileY = 30;
+            break;
+        case "E":
+            tileX = 60;
+            tileY = 30;
+            break;
+        case "F":
+            tileX = 0;
+            tileY = 60;
+            break;
+        case "G":
+            tileX = 30;
+            tileY = 60;
+            break;
+        case "H":
+            tileX = 60;
+            tileY = 60;
+            break;
+        
+        default:
+            tileX = 30;
+            tileY = 30;
+    }
+}
+
+//MAIN LOOP
+function mainLoop(){
+    update();
+    draw();
+    window.requestAnimationFrame(mainLoop);
+}
+
+
+//PLAYER INPUT
+
+function onKeyDown(event){
+    keyState[event.keyCode] = true;
+}
+
+function onKeyUp(event){
+    keyState[event.keyCode] = false;
+}
+
+//UPDATING
+
+function update(){
+    frameCount += 1;
+    if(legalMove){
+        if(keyState[37] && keyState[38]){
+            direction = "NW";
+            if(archieX > 100 && archieY > 100){
+                archieX -= 1;
+                archieY -= 1;
+            } else {
+                mapX += 1;
+                mapY += 1;
+            }
+            
+            
+        } else if (keyState[38] && keyState[39]){
+            direction = "NE";
+            if(archieX < 250 && archieY > 100){
+                archieX += 1;
+                archieY -= 1;
+            } else {
+                mapX -= 1;
+                mapY += 1;
+            }
+            
+        } else if (keyState[37] && keyState[40])  {
+            direction = "SW";
+            if(archieX > 100 && archieY < 200){
+                archieX -= 1;
+                archieY += 1;
+            } else {
+                mapX += 1;
+                mapY -= 1;
+            }
+            
+        } else if (keyState[39] && keyState[40])  {
+            if(archieX < 250 && archieY < 200){
+                archieX += 1;
+                archieY += 1;
+            } else {
+                mapX -= 1;
+                mapY -= 1;
+            }
+
+
+        } else if(keyState[37]){
+            direction = "W";
+            if(archieX > 100){
+                archieX -= 1;
+            } else {
+                mapX += 1;
+            }
+        
+        }  else if (keyState[39]){
+            direction = "E";
+            if(archieX < 350){
+                archieX += 1;
+            } else {
+                mapX -= 1;
+            }
+        
+        } else if (keyState[38]){
+            direction = "N";
+            if (archieY > 100){
+                archieY -= 1;
+            } else {
+                mapY += 1;
+            }
+        
+        } else if (keyState[40]){
+            direction = "S";
+            if (archieY < 200){
+                archieY += 1;
+            } else {
+                mapY -= 1;
+            }
+        }  
+    }
+    relativeX = archieX - mapX;
+    relativeY = archieY - mapY;
+    checkMove(relativeX,relativeY,direction);
+    if(frameCount == 11){
+        frameCount = 0;
+    }
+}
+
+//DRAWING
+function draw(){
+    
+    c.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    //draw the background
+    
+    for(i=0;i<map.length;i++){
+        //console.log("map length: " + map.length);
+        var mapRow = map[i];
+        var mapRowArray = mapRow.split("");
+        for(x=0;x<mapRowArray.length;x++){
+            //console.log("mapRowArray: "+ mapRowArray.length);
+            mapConverter(mapRowArray[x]);
+            c.drawImage(mapImage,tileX,tileY,TILESIZE,TILESIZE,(x*30)+mapX,(i*30)+mapY,TILESIZE,TILESIZE);
+
+        }
+    }
+    //draw Archie
+    if(direction == "W"){
+        if(frameCount >= 0 && frameCount <= 2){
+            c.drawImage(archieImage,0,48,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 3 && frameCount <= 5){
+            c.drawImage(archieImage,16,48,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 6 && frameCount <= 8){
+            c.drawImage(archieImage,32,48,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else {
+            c.drawImage(archieImage,48,48,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        }
+    } else if(direction == "E"){
+        if(frameCount >= 0 && frameCount <= 2){
+            c.drawImage(archieImage,0,32,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 3 && frameCount <= 5){
+            c.drawImage(archieImage,16,32,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 6 && frameCount <= 8){
+            c.drawImage(archieImage,32,32,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else {
+            c.drawImage(archieImage,48,32,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        }
+    } else if(direction == "N"){
+        if(frameCount >= 0 && frameCount <= 2){
+            c.drawImage(archieImage,0,0,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 3 && frameCount <= 5){
+            c.drawImage(archieImage,16,0,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 6 && frameCount <= 8){
+            c.drawImage(archieImage,32,0,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else {
+            c.drawImage(archieImage,48,0,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        }
+       
+    } else if(direction = "S"){
+        if(frameCount >= 0 && frameCount <= 2){
+            c.drawImage(archieImage,0,16,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 3 && frameCount <= 5){
+            c.drawImage(archieImage,16,16,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else if(frameCount >= 6 && frameCount <= 8){
+            c.drawImage(archieImage,32,16,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        } else {
+            c.drawImage(archieImage,48,16,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+        }
+      
+    } else {
+        c.drawImage(archieImage,16,16,ARCHIEWIDTH,ARCHIEHEIGHT,archieX,archieY,ARCHIEWIDTH,ARCHIEHEIGHT);
+    }
+    //c.drawImage(mapImage,0,0);
+    //c.drawImage(mapImage,i*30,x*30,TILESIZE,TILESIZE,tileX,tileY,TILESIZE,TILESIZE);
+    //console.log(keyState);
+}
